b2cde92c6d289cfd501deff1ea9536c8686718f0
Alex Kanellis <akan0033@student.monash.edu>
2023-05-24 19:27:26 +1000

    feat added backend test file and folder this can be run using mocha locally w commmand mocha --recursive backend/tests

---
+++ backend/tests/backend.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Backend Tests', () => {
+  it('should perform a sample test', () => {
+    const sum = 1 + 1;
+    assert.strictEqual(sum, 2);
+  });
+});
